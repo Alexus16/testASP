008dc8d661246ef498fccdfa5e81e16f961c3a63
Alexus16 <zagulin2012@gmail.com>
2022-05-24 02:38:04 +0300

    Initial

---
+++ WebApplication2/Program.cs
@@ -0,0 +1,23 @@
+var builder = WebApplication.CreateBuilder(args);
+
+var app = builder.Build();
+
+app.Map("/index", () => "");
+app.Map("/about/{name}", async (HttpContext context, string name) =>
+{
+    if(name == "Саня")
+    {
+        return "Саня - лох";
+    }
+    else if(name == "Влад")
+    {
+        context.Response.Redirect("https://www.youtube.com/watch?v=dQw4w9WgXcQ");
+        return "";
+    }
+    else
+    {
+        return "Норм чел";
+    }
+});
+
+app.Run();
