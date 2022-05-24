bb41d79aff8f35a3dcc1b6d547e82777ca79c650
Alexus16 <zagulin2012@gmail.com>
2022-05-24 21:17:56 +0300

    -

--- WebApplication2/Program.cs
+++ WebApplication2/Program.cs
@@ -1,6 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
 
-builder.WebHost.UseUrls("http://*:5000;http://localhost:7205");
+builder.WebHost.UseUrls("http://*:5000;http://localhost:7205;http://0.0.0.0:5000");
 
 var app = builder.Build();
 app.Map("/index", () => "");
